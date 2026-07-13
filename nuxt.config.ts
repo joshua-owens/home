79f3355f064a034371d401fb1934be79ffb0bc42
jowens <me@jowens.dev>
2026-07-13 18:33:10 -0500

    chore: scaffold Nuxt 4 app with Nuxt UI, auth-utils, drizzle, vitest

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', 'nuxt-auth-utils'],
+  css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    openrouterApiKey: '',       // NUXT_OPENROUTER_API_KEY
+    researchModel: '',          // NUXT_RESEARCH_MODEL
+    dataDir: './data',          // NUXT_DATA_DIR
+  },
+})
